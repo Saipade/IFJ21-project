51858f8d56320c6773d442888cec89c9a6fb4a08
Saipade <81774545+Saipade@users.noreply.github.com>
2021-10-02 15:01:35 +0200

    Add files via upload

---
+++ scanner.h
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <string.h>
+
+enum keyword {
+    KW_DO, 
+    KW_ELSE,  
+    KW_END,  
+    KW_FUNCTION,
+    KW_GLOBAL,  
+    KW_IF,  
+    KW_INTEGER,  
+    KW_LOCAL,  
+    KW_NIL,
+    KW_NUMBER,  
+    KW_REQUIRE,  
+    KW_RETURN,  
+    KW_STRING,  
+    KW_THEN,  
+    KW_WHILE,
+};
