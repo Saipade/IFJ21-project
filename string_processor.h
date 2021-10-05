01f0abd8fd8bea18cef6815e773ef700c509a6be
Saipade <maksim.tihonov.011@gmail.com>
2021-10-05 21:29:10 +0200

    wtf

--- string_processor.h
+++ string_processor.h
@@ -1,8 +1,18 @@
+/*
+
+*/
+
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
-struct Dynamic_string {
+typedef struct {
 	char *str; 
 	int length; 
-	int size; // allocation size
-};
+	int size; // for mem. allocation
+} Current_string;
+
+bool cs_init (Current_string *str); // initialization
+void cs_clear (Current_string *str); // removal 
+bool cs_read_next (Current_string *str, char s); // add next char
+void cs_mem_free (Current_string *cs); // memory free
--- symtable.h
+++ symtable.h
@@ -2,8 +2,21 @@
 #include <string.h>
 #include <stdio.h> 
 
-struct symbol_BST {
-  int data;
-  BSTnode* Left;
-  BSTnode* Right;
-}
+typedef struct { // binary search tree struct
+  int symbol;
+  symbol_BST *Left;
+  symbol_BST *Right;
+} symbol_BST;
+
+typedef struct { // pointer to head of binary search tree
+  symbol_BST *head;
+} BST;
+
+enum data_type {
+  DT_INTEGER,
+  DT_NUMBER,
+  DT_STRING,
+};
+
+void BST_init (BST* bst);
+void BST_insert_node ()
