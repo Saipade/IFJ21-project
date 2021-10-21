eaebe727f12ffb6c30defbb05fdf9ca3e9c40e32
Maksim Tikhonov <81774545+Saipade@users.noreply.github.com>
2021-10-21 14:40:55 +0200

    Add files via upload

---
+++ string_processor.c
@@ -0,0 +1,55 @@
+/*
+
+*/
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+#include "string_processor.h"
+
+bool ds_init ( Dynamic_string *cStr ) {
+
+    if (!(cStr->str = (char *) malloc( DYNAMIC_STR_STARTING_MEM ))) return false; 
+    
+    cStr->length = 0;
+    cStr->str[cStr->length] = '\0';
+    cStr->size = DYNAMIC_STR_STARTING_MEM;
+
+    return true;
+
+}
+
+bool ds_add_next ( Dynamic_string *cStr, char c ) {
+
+    if (cStr->length + 2 >= cStr->size) {
+        cStr->size = cStr->length + DYNAMIC_STR_INCREASE_MEM;
+        if (!(cStr->str = (char*) realloc( cStr->str, cStr->size ))) return false;
+    }
+
+    cStr->str[cStr->length++] = c;
+    cStr->str[cStr->length] = '\0';
+
+    return true;
+
+}
+
+bool ds_copy ( Dynamic_string *src, Dynamic_string *dst ) {
+
+    if (src->length >= dst->size) {
+        if (!(dst->str = (char*) realloc( dst->str, src->length + 2 ))) return false;
+        dst->size = src->length + 2;
+    }
+    
+    strcpy( dst->str, src->str );
+    dst->length = src->length;
+    
+    return true;
+
+}
+
+void ds_mem_free ( Dynamic_string *cStr ) {
+
+    free( cStr->str );
+
+}
---
+++ string_processor.h
@@ -0,0 +1,57 @@
+/*
+
+*/
+#ifndef STRING_PROCESSOR_H
+#define STRING_PROCESSOR_H
+
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+#define DYNAMIC_STR_STARTING_MEM 20
+#define DYNAMIC_STR_INCREASE_MEM 10
+
+
+// String processing structure
+typedef struct {
+
+	char *str;              // string itself
+	int length;             // length of string
+    int size;               // for memory allocation
+
+} Dynamic_string;
+
+// String processing
+
+/** 
+ * Initialization of dynamic string
+ * @param cStr pointer to current dynamic string
+ * @return
+ */
+bool ds_init ( Dynamic_string *cStr );
+
+/** 
+ * Adds character to the end of dynamic string
+ * @param cStr pointer to current dynamic string
+ * @param c character that will be added
+ * @return
+ */
+bool ds_add_next ( Dynamic_string *cStr, char s );
+
+/** 
+ * Copies string from one dynamic string to another 
+ * @param src pointer to source dynamic string
+ * @param dst pointer to destination dynamic string
+ * @return
+ */
+bool ds_copy ( Dynamic_string *src, Dynamic_string *dst );
+
+/** 
+ * Frees alocated memory of dynamic string
+ * @param cStr pointer to current dynamic string
+ * @return
+ */
+void ds_mem_free ( Dynamic_string *cStr );
+
+#endif
